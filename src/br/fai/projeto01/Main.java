6f867bbc45f1880f42defafeb8f684d19d9b43cb
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-02 19:54:37 -0300

    primeiro commit

---
+++ src/br/fai/projeto01/Main.java
@@ -0,0 +1,19 @@
+package br.fai.projeto01;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main app = new Main();//
+		app.start();// pede para criar um método
+		
+		
+		
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
