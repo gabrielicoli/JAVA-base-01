f11691e239ab0228c78285364693eca388f5fe23
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-02 20:10:25 -0300

    quinto commit

--- src/br/fai/projeto01/Main.java
+++ src/br/fai/projeto01/Main.java
@@ -26,6 +26,14 @@ public class Main {
 		
 		System.out.println("Resultado 2: "+idade);
 		
+		
+		if (idade>20) {
+			System.out.println("A idade é maior que 20");
+		}
+		else {
+			System.out.println("Idade maior que 20");
+		}
+		
 	}
 
 }
